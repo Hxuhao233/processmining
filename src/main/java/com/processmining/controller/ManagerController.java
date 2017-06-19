c086859f3128ab9e40861bef6e8ac7ccd9bb4956
Hxuhao233 <hxh12342005@qq.com>
2017-06-19 11:41:23 +0800

    init 3

--- src/main/java/com/processmining/controller/ManagerController.java
+++ src/main/java/com/processmining/controller/ManagerController.java
@@ -1,9 +1,14 @@
 package com.processmining.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.processmining.service.IManagerService;
+import com.processmining.util.ResponseData;
 
 /**
  * 管理员功能控制器
@@ -12,9 +17,19 @@ import com.processmining.service.IManagerService;
  */
 
 @Controller
+@RequestMapping(value="manager")
 public class ManagerController {
 	
 	@Autowired
 	private IManagerService managerService;
+	
+	@RequestMapping(value="/banUser")
+	public ResponseData banUser(@RequestBody List<Integer> idList){
+		return null;
+		
+	}
+	
+	
+	
 	
 }
